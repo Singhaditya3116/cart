ffe7f2c8eb74b88d3ba7ab92f4e27b01762b6169
aditya3131 <singhaditya3116@gmail.com>
2022-09-13 16:43:27 +0530

    Delete Cart Item added

--- src/CartItem.js
+++ src/CartItem.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 class CartItem extends React.Component{
 
-  increaseQuantity = ()=> {
+//  increaseQuantity = ()=> {
     
     //this.state.quantity++;    this will not reflect in UI as react will not re -render it.
     //we have to tell the react to re-render the changes that we have made. set State re-render implicitly.
@@ -14,7 +14,7 @@ class CartItem extends React.Component{
     */
 
     //second way   -- if previous state required use  this.
-    this.setState((prevState) => {
+ /*   this.setState((prevState) => {
       
       return {
         quantity:prevState.quantity+1
@@ -36,15 +36,16 @@ class CartItem extends React.Component{
       }
     })
   }
+  */
 
   render(){
     const {title,price,quantity} = this.props.product;
     console.log("Cart Item Render");
-    console.log("props:" ,this.props);
+    // console.log("props:" ,this.props);
     return (
       <div className='cart-item'>
         <div className='left-block'>
-          <img alt='hello' src='../public/logo192.png'></img>
+          <img  src=''></img>
         </div>
         <div className='right-block'>
           <h4>{title}</h4>
@@ -52,9 +53,9 @@ class CartItem extends React.Component{
           <div className='quantity'>Qty: {quantity}</div>
           <div className='action-items'>
             {/* Buttons */}
-            <img alt="increase" src='https://cdn-icons-png.flaticon.com/512/3524/3524388.png' onClick = {this.increaseQuantity}></img>
-            <img alt="decrease" src='https://cdn-icons-png.flaticon.com/512/56/56889.png' onClick = {this.decreaseQuantity}></img>
-            <img alt="delete" src='https://cdn-icons-png.flaticon.com/512/484/484611.png'></img>
+            <img alt="increase" src='https://cdn-icons-png.flaticon.com/512/3524/3524388.png' onClick = {() => this.props.onIncreaseQuantity(this.props.product)}></img>
+            <img alt="decrease" src='https://cdn-icons-png.flaticon.com/512/56/56889.png' onClick = {()=> this.props.onDecreaseQuantity(this.props.product)}></img>
+            <img alt="delete" src='https://cdn-icons-png.flaticon.com/512/484/484611.png' onClick={()=>this.props.onDeleteItem(this.props.product.id)}></img>
           </div>
 
         </div>
--- src/CartList.js
+++ src/CartList.js
@@ -32,13 +32,54 @@ class CartList extends React.Component{
     }
   }
 
+  handleIncreaseQuantity = (product)=>{
+    console.log("Product plus clicked ",product);
+    const {products} = this.state;
+    const index = products.indexOf(product)
+    products[index].quantity+=1;
+    this.setState({
+      products:products
+    })
+  }
+
+  handleDecreaseQuantity = (product)=>{
+    console.log("Decrease quantity of product :",product)
+    const {products} = this.state;
+    const index = products.indexOf(product);
+    if(products[index].quantity === 0){
+      return;
+    }
+    products[index].quantity-=1;
+
+    this.setState({
+      products:products
+    })
+  }
+
+  handleDeleteItem = (id)=>{
+    console.log("Delete Clicked ",id);
+    const {products} = this.state;
+    
+    const newProductsArray = products.filter((element)=> element.id!==id);
+
+    this.setState({
+      products:newProductsArray
+    })
+  }
+
   render(){
     console.log("Cart List Render");
     const products = this.state.products;
     return(
       <div className="cart">
         {products.map((product)=>{
-          return <CartItem product={product} key={product.id} />
+          return <CartItem 
+          product={product} 
+          key={product.id} 
+          onIncreaseQuantity ={this.handleIncreaseQuantity}
+          onDecreaseQuantity = {this.handleDecreaseQuantity}
+          onDeleteItem={this.handleDeleteItem}
+          />
         })}
       </div>
     );
