bdf783dd58c247c62d5df0748a06e44973f3e9b6
aditya3131 <singhaditya3116@gmail.com>
2022-09-18 19:42:31 +0530

    increase decrease functionality added real time DB

--- src/App.js
+++ src/App.js
@@ -20,26 +20,47 @@ class App extends React.Component {
   }
 
   componentDidMount(){
+    // firebase
+    //   .firestore()
+    //   .collection("products")
+    //   .get()
+    //   .then((snapshot)=>{
+    //     //console.log("snaps :",snapshot);
+
+    //     const products = snapshot.docs.map((doc)=>{
+    //       console.log("doc data :",doc.data());
+    //       const data = doc.data();
+    //       data.id = doc.id;
+    //       return data;
+    //     })
+
+    //     this.setState({
+    //       products:products,
+    //       loading:false
+    //     })
+
+    //   })
+
     firebase
-      .firestore()
-      .collection("products")
-      .get()
-      .then((snapshot)=>{
-        //console.log("snaps :",snapshot);
-        
-        const products = snapshot.docs.map((doc)=>{
-          //console.log(doc.data());
-          const data = doc.data();
-          data.id = doc.id;
-          return data;
-        })
+    .firestore()
+    .collection("products")
+    .onSnapshot((snapshot)=>{
+      //console.log("snaps :",snapshot);
 
-        this.setState({
-          products:products,
-          loading:false
-        })
-
+      const products = snapshot.docs.map((doc)=>{
+        console.log("doc data :",doc.data());
+        const data = doc.data();
+        data.id = doc.id;
+        return data;
       })
+      //console.log("Component Did Mounted");
+
+      this.setState({
+        products:products,
+        loading:false
+      })
+
+    })
 
   }
 
@@ -47,10 +68,27 @@ class App extends React.Component {
     console.log("Product plus clicked ",product);
     const {products} = this.state;
     const index = products.indexOf(product)
-    products[index].quantity+=1;
-    this.setState({
-      products:products
-    })
+    console.log("Index :",index);
+
+    // products[index].quantity+=1;
+    // this.setState({
+    //   products:products
+    // })
+
+    const docRef = firebase.firestore().collection("products").doc(products[index].id);
+    //console.log(docRef);
+
+    docRef
+      .update({
+        quantity:products[index].quantity+1
+      })
+      .then(()=>{
+        console.log("Product Updated to firebase");
+      })
+      .catch((err)=>{
+        console.log("Error occured :",err);
+      })
+
   }
 
   handleDecreaseQuantity = (product)=>{
@@ -60,11 +98,25 @@ class App extends React.Component {
     if(products[index].quantity === 0){
       return;
     }
-    products[index].quantity-=1;
+    // products[index].quantity-=1;
 
-    this.setState({
-      products:products
-    })
+    // this.setState({
+    //   products:products
+    // })
+    
+
+    const docRef = firebase.firestore().collection("products").doc(products[index].id);
+    //console.log(docRef);
+    docRef
+      .update({
+        quantity:products[index].quantity-1
+      })
+      .then(()=>{
+        console.log("Product Updated to firebase");
+      })
+      .catch((err)=>{
+        console.log("Error occured :",err);
+      })
   }
 
   handleDeleteItem = (id)=>{
@@ -97,6 +149,24 @@ class App extends React.Component {
     return totalPrice;
   }
 
+  addProduct = () =>{
+    firebase
+      .firestore()
+      .collection("products")
+      .add({
+        img:"",
+        title:"Washing MAchine",
+        price:10000,
+        quantity:1
+      })
+      .then((docRef)=>{
+        console.log("New Doc Added :",docRef);
+      })
+      .catch((err)=>{
+        console.log("Error occured :",err);
+      })
+  }
+
   render(){
     const {products,loading} = this.state;
 
@@ -105,6 +175,7 @@ class App extends React.Component {
       <Navbar
       productCount = {this.productCount()}
       />
+      {/* <button className="add-product-btn" onClick={this.addProduct}>Add a product</button> */}
       <CartList
       onIncreaseQuantity ={this.handleIncreaseQuantity}
       onDecreaseQuantity = {this.handleDecreaseQuantity}
